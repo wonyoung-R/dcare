d91570f689d7899e456e50d63aab47178a688957
Wonyoung Ryu <grizrider@Wonyoungui-MacBookAir.local>
2025-04-23 13:02:23 +0900

    Fix: restore App component as root component instead of Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import App from './App.jsx';
 import './index.css';
 import './i18n/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <Home />
+      <App />
     </Router>
   </React.StrictMode>
 ); 
