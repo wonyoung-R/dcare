4e8ad2865ef6433fa3d4f638592f72663feeba00
Wonyoung Ryu <grizrider@Wonyoungui-MacBookAir.local>
2025-04-23 11:59:27 +0900

    Update Home component import path with jsx extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import './index.css';
 import './i18n/i18n';
 
