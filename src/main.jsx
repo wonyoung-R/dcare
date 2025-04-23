bf6a01588387bef97f89528b9e9fd8049da595d0
Wonyoung Ryu <grizrider@Wonyoungui-MacBookAir.local>
2025-04-23 11:53:16 +0900

    Replace App with Home component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import Home from './pages/Home';
 import './index.css';
 import './i18n/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Home />
     </Router>
   </React.StrictMode>
 ); 
